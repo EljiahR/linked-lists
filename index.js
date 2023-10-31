78722d0591fff6cbb8cd8497d917834a8bc22086
Elijah <kcerhajile@gmail.com>
2023-10-31 13:56:15 -0400

    Added append functionality

--- index.js
+++ index.js
@@ -1,12 +1,59 @@
-function createLinkedList (){
+function createNode(value = null,nextNode = null){
     return {
-
+        value: value,
+        nextNode: nextNode
     }
 };
 
-function createNode(){
+function createLinkedList(){
     return {
-        
+        head: null,
+
+        append(value){
+            if(!this.head){
+                this.head = createNode(value)
+            }else {
+                if(!this.head.nextNode){
+                    this.head.nextNode = createNode(value)
+                }else{
+                    tail = this.head.nextNode;
+                    while(tail.nextNode){
+                        tail = tail.nextNode;
+                    }
+                    tail.nextNode = createNode(value);
+                }
+            }
+        },
+        getHead(){
+            return this.head;
+        },
+
+        tail(){
+            let tail = this.head.nextNode;
+            while(tail.nextNode){
+                tail = tail.nextNode
+            }
+            return tail;
+        },
+
+        prepend(value){},
+
+        size(){},
+
+        at(index){},
+
+        pop(){},
+
+        contains(value){},
+
+        find(value){},
+
+        toString(){}
     }
 }
 
+const list = createLinkedList();
+list.append(2);
+list.append(3);
+list.append(1)
+console.log()
